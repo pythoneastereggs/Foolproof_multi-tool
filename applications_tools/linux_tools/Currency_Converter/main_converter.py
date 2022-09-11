0eb6009559cb120dbd4b2086d568f43cd1d015b1
Dr Efficient <aggelosmoraitis1@gmail.com>
2022-09-11 11:49:59 +0300

    curency converter done

--- applications_tools/linux_tools/Currency_Converter/main_converter.py
+++ applications_tools/linux_tools/Currency_Converter/main_converter.py
@@ -1,106 +1,148 @@
 
 def currency_converter():   ## this is a test for a gpg key
-    curr_api = "ac28563aabf28f88416d3ad591d4cf01"
-    while True: 
-        print("Πληκτρολόγησε 1 για Δολλάριο")
-        print("Πληκτρολόγησε 2 για Ευρό")
-        print("Πληκτρολόγησε 3 για Αγγλική λίρα")
-        print("Πληκτρολόγησε 4 για Καναδικό δολλάριο")
-        print("Πληκτρολόγησε 5 για Κινέζικο Yuan Renminbi")
-        print("Πληκτρολόγησε 6 για Ιαπωνικό Yen")
-        print("Πληκτρολόγησε q ή Q για έξοδο")
-        choice1 = input(int("Επίλεξε το αρχικό νόμισμα σου"))
-        ammount = input(float("Πληκτρολόγησε το ποσό που θέλεις να μετατρέψεις"))
-        if choice1 == 'q' or choice1 == 'Q' :
+    import requests
+    from bs4 import BeautifulSoup
+    from applications_tools.linux_tools.g_lib.general_library import users_inputs
+    # well time for some explanation about what is happening here
+    # this is a curency Converter and uses the https://www.unitconverters.net/ site to grab the most recent values of the USD to whatever curency it have
+
+
+    def find_amount(url, replace1, replace2, replace3, replace4):
+        print(url)
+        response_from = requests.get(str(url))
+        response_to = requests.get(str(url))
+
+        from_result = BeautifulSoup(response_from.text, "html.parser")
+        to_result = BeautifulSoup(response_to.text, "html.parser")
+
+        replace1 = replace1 + " "  # web scraping for the amounts
+        from_finder = from_result.find_all("table")[0].parent.find_all("td")[8].find_all("input")[0]['value'].replace(replace1, "").replace(replace2, "").replace("[", "").replace("]", "")
+        to_finder = to_result.find_all("table")[0].parent.find_all("td")[8].find_all("input")[1]['value'].replace(replace3, "").replace(replace4, "").replace("[", "").replace("]", "")
+        finder = float(from_finder) / float(to_finder)
+        return finder
+
+
+    while True:
+        euro = False
+        usd = False
+        cad = False
+        yen = False
+        yuan = False
+        gbp = False
+
+        print("""    0-exit
+            1- Europe euro
+            2- United States dollar
+            3- Canadian dollar
+            4- Japanese yen
+            5- Chinese yuan
+            6- British Pound Sterling""")
+        users_input = users_inputs(0, 6)
+
+        if users_input == 0:
             break
-        elif choice1 == 1:
-            choice2 = input(int("Επίλεξε το νόμισμα που θέλεις να μετατρέψεις το αρχικό σου"))
-            if choice2 == 1:
-                print("Λάθος επιλογή")
-                
-            elif choice2 == 2:
-                Result = ammount*0.898230
-            elif choice2 == 3:
-                Result = ammount*0.75967983
-            elif choice2 == 4:
-                Result = ammount*1.2485194
-            elif choice2 == 5:
-                Result = ammount*6.3514865
-            else:
-                Result = ammount*121.80500
-        elif choice1 == 2:
-            choice2 = input(int("Επίλεξε το νόμισμα που θέλεις να μετατρέψεις το αρχικό σου"))
-            if choice2 == 1:
-                Result = ammount*0.898230
-            elif choice2 == 2:
-                print("Λάθος επιλογή")
-            elif choice2 == 3:
-                Result = ammount*0.85
-            elif choice2 == 4: 
-                Result = ammount*1.39 
-            elif choice2 == 5:
-                Result = ammount*7.0624671
-            else: 
-                Result = ammount*135.57
-        elif choice1 == 3:
-            choice2 = input(int("Επίλεξε το νόμισμα που θέλεις να μετατρέψεις το αρχικό σου"))
-            if choice2 == 1:
-                Result = ammount*1.32
-            elif choice2 == 2:
-                Result = ammount*1.18
-            elif choice2 == 3:
-                print("Λάθος επιλογή") 
-            elif choice2 == 4:
-                Result = ammount*1.64
-            elif choice2 == 5:
-                Result = ammount*8.3576397
-            else: 
-                Result = ammount*160.32
-        elif choice1 == 4:
-            choice2 = input(int("Επίλεξε το νόμισμα που θέλεις να μετατρέψεις το αρχικό σου"))
-            if choice2 == 1:
-                Result = ammount*0.80
-            elif choice2 == 2:
-                Result = ammount*0.72
-            elif choice2 == 3:
-                Result = ammount*0.61
-            elif choice2 == 4:
-                print("Λάθος επιλογή")
-            elif choice2 == 5:
-                Result = ammount*5.0961898
-            else: 
-                Result = ammount*97.77
-        elif choice1 == 5:
-            choice2 = input(int("Επίλεξε το νόμισμα που θέλεις να μετατρέψεις το αρχικό σου"))
-            if choice2 == 1:
-                Result = ammount*0.15742974
-            elif choice2 == 2:
-                Result = ammount*0.14149625
-            elif choice2 == 3:
-                Result = ammount*0.1196892
-            elif choice2 == 4:
-                Result = ammount*0.19613971
-            elif choice2 == 5:
-                print("Λάθος επιλογή")
-            else:
-                Result = ammount*19.187744
-        else: 
-            choice2 = input(int("Επίλεξε το νόμισμα που θέλεις να μετατρέψεις το αρχικό σου"))
-            if choice2 == 1:
-                Result = ammount*0.0082
-            elif choice2 == 2:
-                Result = ammount*0.0074
-            elif choice2 == 3:
-                Result = ammount*0.0062
-            elif choice2 == 4:
-                Result = ammount*0.010
-            elif choice2 == 5:
-                Result = ammount*0.052120939
-            else:
-                print("Λάθος επιλογή") 
-        print("Το μετατροποιημένο ποσό είναι", Result)          
-            
-                
-                
-        
-        
+
+        elif users_input == 1:
+            euro = True
+            main = "eur"
+            replace1 = "EUR"  # replace 1 and 2 is needed for replacing some characters from the string to find the amount
+            replace2 = "Euro"
+        elif users_input == 2:
+            usd = True
+            main = "usd"
+            replace1 = "USD"
+            replace2 = "United States Dollar"
+        elif users_input == 3:
+            cad = True
+            main = "cad"
+            replace1 = "CAD"
+            replace2 = "Canadian Dollar"
+        elif users_input == 4:
+            yen = True
+            main = "jpy"
+            replace1 = "JPY"
+            replace2 = "Japanese Yen"
+        elif users_input == 5:
+            yuan = True
+            main = "cny"
+            replace1 = "CNY"
+            replace2 = "Chinese Yuan"
+        elif users_input == 6:
+            gbp = True
+            main = "gbp"
+            replace1 = "GBP"
+            replace2 = "British Pound Sterling"
+
+        while True:
+            num_euro = -1
+            num_usd = -1
+            num_cad = -1
+            num_yen = -1
+            num_yuan = -1
+            num_gbp = -1
+            i = 0
+
+            if not euro:
+                i += 1
+                print("    " + str(i) + "- to convert to euro ")
+                num_euro = i
+
+            if not usd:
+                i += 1
+                print("    " + str(i) + "- to convert to usd ")
+                num_usd = i
+
+            if not cad:
+                i += 1
+                print("    " + str(i) + "- to convert to cad ")
+                num_cad = i
+
+            if not yen:
+                i += 1
+                print("    " + str(i) + "- to convert to yen ")
+                num_yen = i
+
+            if not yuan:
+                i += 1
+                print("    " + str(i) + "- to convert to yuan ")
+                num_yuan = i
+
+            if not gbp:
+                i += 1
+                print("    " + str(i) + "- to convert to British Pound Sterling ")
+                num_gbp = i
+
+            print("    0- exit")
+
+            users_input = users_inputs(0, i)
+
+            if users_input == 0:
+                break
+            elif users_input == num_euro:  
+                url = "https://www.unitconverters.net/currency/" + str(main) + "-to-eur.htm"
+
+                amount = find_amount(url, replace1, replace2, "EUR", "Euro")  # URL and the replace 3 and 4 values
+
+            elif users_input == num_usd:
+                url = "https://www.unitconverters.net/currency/" + str(main) + "-to-usd.htm"
+
+                amount = find_amount(url, replace1, replace2, "USD", "United States Dollar")
+
+            elif users_input == num_cad:
+                url = "https://www.unitconverters.net/currency/" + str(main) + "-to-cad.htm"
+                amount = find_amount(url, replace1, replace2, "CAD", "Canadian Dollar")
+
+            elif users_input == num_yen:
+                url = "https://www.unitconverters.net/currency/" + str(main) + "-to-jpy.htm"
+                amount = find_amount(url, replace1, replace2, "JPY", "Japanese Yen")
+
+            elif users_input == num_yuan:
+                url = "https://www.unitconverters.net/currency/" + str(main) + "-to-cny.htm"
+                amount = find_amount(url, replace1, replace2, "CNY", "Chinese Yuan")
+
+            elif users_input == num_gbp:
+                url = "https://www.unitconverters.net/currency/" + str(main) + "-to-gbp.htm"
+                amount = find_amount(url, replace1, replace2, "GBP", "British Pound Sterling")
+
+            amount = float(amount) * int(input("give me the amount you want to convert: "))
+            print("The amount is: " + str(amount))
