b558f8b9a05200ec049d75aa8f005b0f8079f1d1
Dr Efficient <aggelosmoraitis1@gmail.com>
2022-09-14 22:33:09 +0300

    Update main_games.py

--- applications_tools/games/main_games.py
+++ applications_tools/games/main_games.py
@@ -7,7 +7,8 @@ from applications_tools.games.Hangman.Main_Hangman import Hangman_game
 from applications_tools.games.Dice_roller.Dice_Roller import DiceRoller
 
 def games():
-    print("""So you want to play some games huh??
+    while True:
+        print("""So you want to play some games huh??
 Here is what i got for you:
     1- 2048
     2- Coin flip
@@ -16,17 +17,19 @@ Here is what i got for you:
     5- Dice_Roller
     6- tic tac toe
     0- exit""")
-    users_input = users_inputs(0,6)
+        users_input = users_inputs(0,6)
 
-    if users_input == 1:
-        os.system("python3 $(pwd)/applications_tools/games/2048/puzzle.py")
-    elif users_input == 2:
-        coin_flip()
-    elif users_input == 3:
-        Hangman_game()
-    elif users_input == 4:
-        os.system("python3 $(pwd)/applications_tools/games/Pong/Main_Pong.py")
-    elif users_input == 5:
-        DiceRoller()
-    elif users_input == 6:
-        os.system("python3 $(pwd)/applications_tools/games/Tic_Tac_Toe.py")
+        if users_input == 0:
+            break        
+        elif users_input == 1:
+            os.system("python3 $(pwd)/applications_tools/games/2048/puzzle.py")
+        elif users_input == 2:
+            coin_flip()
+        elif users_input == 3:
+            Hangman_game()
+        elif users_input == 4:
+            os.system("python3 $(pwd)/applications_tools/games/Pong/Main_Pong.py")
+        elif users_input == 5:
+            DiceRoller()
+        elif users_input == 6:
+            os.system("python3 $(pwd)/applications_tools/games/Tic_Tac_Toe.py")
